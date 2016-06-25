0243b654119ee74bd34738077f9a1976b745df54
Dionatan Moura <dionatanmouramarques@gmail.com>
2016-06-24 23:35:28 -0300

    Characters Show More

--- js/controllers.js
+++ js/controllers.js
@@ -6,4 +6,12 @@ controllers.controller("CharactersController", function($scope, Characters) {
     Characters.all().then(function(resolve) {
         $scope.characters = resolve;
     });
+    
+    $scope.showMore = function() {
+        var offset = $scope.characters.length;
+        Characters.all(offset).then(function(resolve) {
+            Array.prototype.push.apply($scope.characters, resolve);
+            console.log($scope.characters);
+        });
+    }
 });
--- js/services.js
+++ js/services.js
@@ -2,12 +2,13 @@ var services = angular.module("marvel.services", []);
 
 services.factory("Characters", function($http, $q, marvelApi) {
     return {
-        all: function() {
+        all: function(offset) {
             var deferred = $q.defer();
             $http({
                 method: "GET",
                 url: marvelApi.URL + "/characters",
                 params: {
+                    offset: offset | 0,
                     apikey: marvelApi.KEY,
                     ts: marvelApi.TS,
                     hash: marvelApi.HASH
