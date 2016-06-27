ae371e68bbafa6772aa13d8ac5da64e0f38cda17
antonioallan <antonioallan.santos@gmail.com>
2016-06-27 20:49:01 -0300

    Teste

--- js/directives.js
+++ js/directives.js
@@ -3,6 +3,6 @@ var directives = angular.module("marvel.directives", []);
 directives.directive("comicCard", function(){
 	return {
 			  restrict: 'E', 
-		      templateUrl: "partials/comic-card.html",
+		      templateUrl: "partials/comic-card.html"
 	};
 })
